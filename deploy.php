14c05cd2cf8ed7ce7c407898a13e30a98b85beb0
Bit Player <shikanchen@users.noreply.github.com>
2022-04-12 16:02:11 -0700

    often log

--- deploy.php
+++ deploy.php
@@ -1,62 +1,5 @@
 <?php
 
-// Forked from https://gist.github.com/1809044
-// Available from https://gist.github.com/nichtich/5290675#file-deploy-php
-
-$TITLE   = 'Git Deployment Hamster';
-$VERSION = '0.11';
-
-echo <<<EOT
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>$TITLE</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
-  o-o    $TITLE
- /\\"/\   v$VERSION
-(`=*=') 
- ^---^`-.
-EOT;
-
-// Check whether client is allowed to trigger an update
-
-$allowed_ips = array(
-	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', 'https://api.github.com/users/Have-Faith-In-Data'// GitHub
-	'195.37.139.','193.174.', // VZG
-    '76.176.65.150' // myself
-);
-$allowed = false;
-
-$headers = apache_request_headers();
-
-if (@$headers["X-Forwarded-For"]) {
-    $ips = explode(",",$headers["X-Forwarded-For"]);
-    $ip  = $ips[0];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-
-foreach ($allowed_ips as $allow) {
-    if (stripos($ip, $allow) !== false) {
-        $allowed = true;
-        break;
-    }
-}
-
-if (!$allowed) {
-	header('HTTP/1.1 403 Forbidden');
- 	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
-    echo "</pre>\n</body>\n</html>";
-    exit;
-}
-
-    flush();
-
-// Actually run the update
-
 $commands = array(
 	'echo $PWD',
 	'whoami',
@@ -65,7 +8,7 @@ $commands = array(
 	'git submodule sync',
 	'git submodule update',
 	'git submodule status',
-//  'test -e /usr/share/update-notifier/notify-reboot-required && echo "system restart required"',
+    'test -e /usr/share/update-notifier/notify-reboot-required && echo "system restart required"',
 );
 
 $output = "\n";
@@ -88,6 +31,3 @@ foreach($commands AS $command){
 echo $output; 
 
 ?>
-</pre>
-</body>
-</html>
