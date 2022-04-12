8104c63e728f10c0c92728784988a7c37f72feda
Bit Player <shikanchen@users.noreply.github.com>
2022-04-12 15:36:06 -0700

    access granted

--- deploy.php
+++ deploy.php
@@ -24,7 +24,7 @@ EOT;
 // Check whether client is allowed to trigger an update
 
 $allowed_ips = array(
-	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', // GitHub
+	'207.97.227.', '50.57.128.', '108.171.174.', '50.57.231.', '204.232.175.', '192.30.252.', 'https://api.github.com/users/Have-Faith-In-Data'// GitHub
 	'195.37.139.','193.174.' // VZG
 );
 $allowed = false;
