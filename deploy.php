fad681b085836569fe68ff98798f3340288d9b8c
Bit Player <shikanchen@users.noreply.github.com>
2022-04-12 15:52:53 -0700

    often log

--- deploy.php
+++ deploy.php
@@ -70,21 +70,23 @@ $commands = array(
 $output = "\n";
 
 $log = "####### ".date('Y-m-d H:i:s'). " #######\n";
+file_put_contents ('deploy-log.txt', $log, FILE_APPEND);
 
 foreach($commands AS $command){
     // Run it
     $tmp = shell_exec("$command 2>&1");
     // Output
     $output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+    
     $output .= htmlentities(trim($tmp)) . "\n";
 
-    $log  .= "\$ $command\n".trim($tmp)."\n";
+    $log  = "\$ $command\n".trim($tmp)."\n";
+    file_put_contents ('deploy-log.txt', $log, FILE_APPEND);
 }
-
-$log .= "\n";
-
-file_put_contents ('deploy-log.txt',$log,FILE_APPEND);
 
 echo $output; 
 
 ?>
+</pre>
+</body>
+</html>
