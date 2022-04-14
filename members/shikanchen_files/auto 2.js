31fcb2b8e5318682ecb400d674edd7b56d104071
Bit Player <shikanchen@users.noreply.github.com>
2022-04-13 22:15:21 -0700

    rename

---
+++ members/shikanchen_files/auto 2.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+    
+// TODO: not really good looking, need to modify, unused right now
+//  let github_info = {
+//      'username': 'shikanchen',
+//      'repos' : ['language-modeling-cometh', '2048AI', 'Terminal', 'GraphModel']
+//  };
+//  
+//  for (let i = 0; i < github_info['repos'].length; i++) {
+//      $( "#repos" ).append('<object class="repo" type="image/svg+xml" data=""></object>')
+//  }
+//  
+//  $( ".repo" ).each(function( index ) {
+//      console.log( index + ": " + $( this ).text() );
+//      $( this ).attr("data","https://gh-card.dev/repos/"+github_info['username']+"/"+github_info['repos'][index]+".svg?fullname=&link_target=_blank");
+//  });
+});
