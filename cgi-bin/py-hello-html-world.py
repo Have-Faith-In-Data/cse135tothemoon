40c19dbdbff01d8accea540036c38438195b16d2
HT <jasonzht233@gmail.com>
2022-04-22 14:47:40 -0700

    part1 python
    add python file to cgi-bin

---
+++ cgi-bin/py-hello-html-world.py
@@ -0,0 +1,24 @@
+#!/usr/bin/py
+from datetime import datetime
+import socket
+
+print( "Cache-Control: no-cache\n")
+print( "Content-type: text/html\n\n")
+print( "<html>")
+print( "<head>")
+print( "<title>Hello, Perl!</title>")
+print( "</head>")
+print( "<body>")
+
+print( "<h1>Thomas was here - Hello, Perl!</h1>")
+print( "<p>This page was generated with the Perl programming langauge</p>")
+
+date = datetime.now()  
+print( "<p>Current Time: {d}</p>".format(d = date))
+
+# IP Address is an environment variable when using CGI
+address = socket.gethostbyname(socket.gethostname())
+print( "<p>Your IP Address: {a}</p>".format(a=address))
+
+print( "</body>")
+print( "</html>")
