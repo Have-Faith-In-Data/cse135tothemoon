dfc1cf9539f1a621a9579500b3a5756df3b81350
HT <jasonzht233@gmail.com>
2022-04-25 20:48:58 -0700

    Update py-env.py
    test_01

---
+++ cgi-bin/py-env.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+import socket
+import os 
+
+print( "Cache-Control: no-cache\n")
+print( "Content-type: text/html\n\n")
+print( "<html>")
+print( "<head>")
+print( "<title>Environment Variables</title>")
+print( "</head>")
+print( "<body>")
+
+print( "<h1>Environment Variables</h1>")
+
+for i in os.environ.keys(): 
+    print(i,":",os.environ[i],"<br />\n" )
+
+print( "</body>")
+print( "</html>")
